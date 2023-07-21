c92ef04e44be4130212c004d7990224c464b9c7b
lwang41 <lwang41@jlr1cnxm7jxyq.shanghai.jlrint.com>
2023-07-21 16:36:42 +0800

    run prettier then run eslint

--- scripts/idGenerator.js
+++ scripts/idGenerator.js
@@ -1,5 +1,5 @@
-import * as idLib from 'uuid'
+import * as idLib from "uuid";
 
-const {v1, v3, v4, v5} = idLib
+const { v1, v3, v4, v5 } = idLib;
 
-export default {v1, v3, v4, v5}
+export default { v1, v3, v4, v5 };
