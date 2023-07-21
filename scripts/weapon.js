b87e09609c512fa02c14846dc0f7136f835d69f8
lwang41 <lwang41@jlr1cnxm7jxyq.shanghai.jlrint.com>
2023-07-21 16:29:02 +0800

    test

--- scripts/weapon.js
+++ scripts/weapon.js
@@ -1,8 +1,8 @@
 import idGen from './idGenerator.js'
 
 const { v4 } = idGen
-const id1 = v4()
+  const id1 = v4()
 const id2 = v4()
-const id3 = v4()
+   const id3 = v4()
 
 console.log(id1, id2, id3)
