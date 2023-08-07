8ed938c47e1e6d28d9cd4bb1d909bb1c8ef0efe0
lwang41 <lwang41@jlr1cnxm7jxyq.shanghai.jlrint.com>
2023-08-07 14:29:55 +0800

    fix npm run lint command

--- scripts/weapon.ts
+++ scripts/weapon.ts
@@ -6,4 +6,9 @@ const id2 = v4();
 const id3 = v4();
 const id4 = v4();
 
-console.log(id1, id2, id3, id4);
+const getID = (param: number):string => {
+    const IdArray = [id1, id2, id3, id4]
+    return IdArray[param % 4]
+}
+
+console.log(getID(2), getID(7));
